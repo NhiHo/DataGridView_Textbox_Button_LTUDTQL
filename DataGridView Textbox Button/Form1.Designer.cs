ccf2b65c9fdfc4b49f4b784fd7c523e7bec38f01
Hồ Xuân Ý Nhi <nhi1296@gmail.com>
2017-03-12 19:48:29 +0700

    BAI 3
    ...

---
+++ DataGridView Textbox Button/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace DataGridView_Textbox_Button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvKhoa = new System.Windows.Forms.DataGridView();
+            this.lblMaKhoa = new System.Windows.Forms.Label();
+            this.lblTenKhoa = new System.Windows.Forms.Label();
+            this.lblSDT = new System.Windows.Forms.Label();
+            this.txtMaKhoa = new System.Windows.Forms.TextBox();
+            this.txtTenKhoa = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.btnTaoMoi = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhoa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvKhoa
+            // 
+            this.dgvKhoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhoa.Location = new System.Drawing.Point(12, 127);
+            this.dgvKhoa.Name = "dgvKhoa";
+            this.dgvKhoa.Size = new System.Drawing.Size(499, 176);
+            this.dgvKhoa.TabIndex = 1;
+            this.dgvKhoa.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKhoa_CellClick);
+            // 
+            // lblMaKhoa
+            // 
+            this.lblMaKhoa.AutoSize = true;
+            this.lblMaKhoa.Location = new System.Drawing.Point(10, 15);
+            this.lblMaKhoa.Name = "lblMaKhoa";
+            this.lblMaKhoa.Size = new System.Drawing.Size(50, 13);
+            this.lblMaKhoa.TabIndex = 3;
+            this.lblMaKhoa.Text = "Mã Khoa";
+            // 
+            // lblTenKhoa
+            // 
+            this.lblTenKhoa.AutoSize = true;
+            this.lblTenKhoa.Location = new System.Drawing.Point(10, 50);
+            this.lblTenKhoa.Name = "lblTenKhoa";
+            this.lblTenKhoa.Size = new System.Drawing.Size(54, 13);
+            this.lblTenKhoa.TabIndex = 4;
+            this.lblTenKhoa.Text = "Tên Khoa";
+            // 
+            // lblSDT
+            // 
+            this.lblSDT.AutoSize = true;
+            this.lblSDT.Location = new System.Drawing.Point(224, 50);
+            this.lblSDT.Name = "lblSDT";
+            this.lblSDT.Size = new System.Drawing.Size(29, 13);
+            this.lblSDT.TabIndex = 5;
+            this.lblSDT.Text = "SĐT";
+            // 
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.Location = new System.Drawing.Point(70, 12);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(143, 20);
+            this.txtMaKhoa.TabIndex = 6;
+            // 
+            // txtTenKhoa
+            // 
+            this.txtTenKhoa.Location = new System.Drawing.Point(70, 47);
+            this.txtTenKhoa.Name = "txtTenKhoa";
+            this.txtTenKhoa.Size = new System.Drawing.Size(143, 20);
+            this.txtTenKhoa.TabIndex = 7;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(259, 47);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(143, 20);
+            this.txtSDT.TabIndex = 8;
+            // 
+            // btnTaoMoi
+            // 
+            this.btnTaoMoi.Location = new System.Drawing.Point(13, 83);
+            this.btnTaoMoi.Name = "btnTaoMoi";
+            this.btnTaoMoi.Size = new System.Drawing.Size(75, 23);
+            this.btnTaoMoi.TabIndex = 10;
+            this.btnTaoMoi.Text = "Tạo mới";
+            this.btnTaoMoi.UseVisualStyleBackColor = true;
+            this.btnTaoMoi.Click += new System.EventHandler(this.btnTaoMoi_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(94, 83);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 11;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.Location = new System.Drawing.Point(175, 83);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(75, 23);
+            this.btnCapNhat.TabIndex = 12;
+            this.btnCapNhat.Text = "Cập nhật";
+            this.btnCapNhat.UseVisualStyleBackColor = true;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(256, 83);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 13;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(408, 83);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 14;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 315);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnCapNhat);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnTaoMoi);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtTenKhoa);
+            this.Controls.Add(this.txtMaKhoa);
+            this.Controls.Add(this.lblSDT);
+            this.Controls.Add(this.lblTenKhoa);
+            this.Controls.Add(this.lblMaKhoa);
+            this.Controls.Add(this.dgvKhoa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhoa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvKhoa;
+        private System.Windows.Forms.Label lblMaKhoa;
+        private System.Windows.Forms.Label lblTenKhoa;
+        private System.Windows.Forms.Label lblSDT;
+        private System.Windows.Forms.TextBox txtMaKhoa;
+        private System.Windows.Forms.TextBox txtTenKhoa;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Button btnTaoMoi;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnCapNhat;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ DataGridView Textbox Button/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataGridView_Textbox_Button
+{
+    public partial class Form1 : Form
+    {
+        string SQL;
+        SqlConnection cn;
+        SqlDataAdapter da;
+        string connsstring = @"Data Source=NHIHO-PC;Initial Catalog=K48;Integrated Security=True";
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDL()
+        {
+            cn = new SqlConnection(connsstring);
+            SQL = "SELECT * FROM Khoa";
+            da = new SqlDataAdapter(SQL, cn);
+            ds = new DataSet("Khoa");
+            da.Fill(ds, "Khoa");
+            dgvKhoa.DataSource = ds;
+            dgvKhoa.DataMember = "Khoa";
+            cn.Close();
+            cn.Dispose();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDL();
+            
+        }
+
+        private void dgvKhoa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row = dgvKhoa.Rows[e.RowIndex];
+                txtMaKhoa.Text = row.Cells[0].Value.ToString();
+                txtTenKhoa.Text = row.Cells[1].Value.ToString();
+                txtSDT.Text = row.Cells[2].Value.ToString();
+                txtMaKhoa.Enabled = false;
+                btnLuu.Enabled = false;
+            }
+            catch (Exception)
+            { 
+
+            }
+        }
+
+
+        private void btnTaoMoi_Click(object sender, EventArgs e)
+        {
+            txtMaKhoa.Clear();
+            txtMaKhoa.Focus();
+            txtTenKhoa.Clear();
+            txtSDT.Clear();
+            txtMaKhoa.Enabled = true;
+            btnCapNhat.Enabled = true;
+            btnLuu.Enabled = true;
+            btnXoa.Enabled = false;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            SQL = "INSERT INTO Khoa VALUES (N'"+txtMaKhoa.Text.ToString()+"',N'"+txtTenKhoa.Text.ToString()+"',N'"+txtSDT.Text.ToString()+"')";
+            cn = new SqlConnection(connsstring);
+            SqlCommand cmd = new SqlCommand(SQL, cn);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            LoadDL();
+            cn.Close();
+            cn.Dispose();
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQL = "UPDATE Khoa SET TenKhoa = N'" + txtTenKhoa.Text.ToString() + ", SDT = N'" + txtSDT.Text.ToString() + "' WHERE MaKhoa = N'" + txtMaKhoa.Text.ToString() + "')";
+                cn = new SqlConnection(connsstring);
+                SqlCommand cmd = new SqlCommand(SQL, cn);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                LoadDL();
+                cn.Close();
+                cn.Dispose();
+            }
+            catch
+            {
+                MessageBox.Show("Có lỗi","Chú ý",MessageBoxButtons.OKCancel,MessageBoxIcon.Question);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult k = MessageBox.Show("Bạn có chắc chắn muốn xóa không?", "Chú ý", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (k == DialogResult.OK)
+            {
+                SQL = "DELETE FROM Khoa WHERE MaKhoa = N'" + txtTenKhoa.Text + "'";
+                cn = new SqlConnection(connsstring);
+                SqlCommand cmd = new SqlCommand(SQL, cn);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                LoadDL();
+                cn.Close();
+                cn.Dispose();
+            }
+        }
+
+
+        
+
+
+    }
+}
